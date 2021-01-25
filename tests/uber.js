ca36d667b1eb2efe4e12bc7c971f7f1caaa11ec9
Farès <faresboukhezna@gmail.com>
2021-01-25 11:14:17 +0100

    5 first tests

--- tests/uber.js
+++ tests/uber.js
@@ -1,16 +1,83 @@
-describe('Ecosia example', function() {
+describe('Uber tests', function() {
 
-    beforeEach(browser => browser.url('https://www.ecosia.org/'));
+    beforeEach(browser => browser.url('https://www.uber.com/fr/fr/'));
 
-    test('Demo test ecosia.org', function (browser) {
+    test('Open Uber website', function (browser) {
       browser
         .waitForElementVisible('body')
-        .assert.titleContains('Ecosia')
-        .assert.visible('input[type=search]')
-        .setValue('input[type=search]', 'nightwatch')
-        .assert.visible('button[type=submit]')
-        .click('button[type=submit]')
-        .assert.containsText('.mainline-results', 'Nightwatch.js')
+        .assert.elementPresent('head')
+        .assert.elementPresent('title')
         .end();
     });
+
+    test('Check block login in the header wrapper', function (browser) {
+      browser
+        .waitForElementVisible('body')
+        .useXpath()
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[2]/button')
+        .end();
+    });
+
+    test('Check login block in two sections', function (browser) {
+      browser
+        .useCss()
+        .waitForElementVisible('body')
+        .useXpath()
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .click('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[1]/a')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[3]/a')
+        .end();
+    });
+
+    test('Check login chauffeur section', function (browser) {
+      browser
+        .useCss()
+        .waitForElementVisible('body')
+        .useXpath()
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .click('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[1]/a')
+        .click('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[1]/a')
+        .useCss()
+        .waitForElementVisible('body')
+        .assert.urlContains('https://auth.uber.com/login/')
+        .useXpath()
+        .assert.elementPresent('//*[@id="useridInput"]')
+        .assert.elementPresent('//*[@id="app-body"]/div/div[1]/form/div[2]/button')
+        .end();
+    });
+
+    test('Check login passager section', function (browser) {
+      browser
+        .useCss()
+        .waitForElementVisible('body')
+        .useXpath()
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .click('//*[@id="main"]/nav/div/ul[3]/li[1]/button')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[3]/a')
+        .click('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[3]/a')
+        .assert.elementPresent('//*[@id="mobile"]')
+        .assert.elementPresent('//*[@id="next-button"]')
+        .assert.elementPresent('//*[@id="app-content"]/div/div/div/div/div/div/div/div[2]/div/a')
+        .click('//*[@id="app-content"]/div/div/div/div/div/div/div/div[2]/div/a')
+        .waitForElementVisible('/html/body')
+        .assert.elementPresent('//*[@id="app-content"]/div/div/div/div/div/a[1]')
+        .assert.elementPresent('//*[@id="googleLoginButton"]')
+        .end();
+    });
+    
+    test('Check Register block in two sections', function (browser) {
+      browser
+        .useCss()
+        .waitForElementVisible('body')
+        .useXpath()
+        .assert.elementPresent('//*[@id="main"]/nav/div/ul[3]/li[2]/button')
+        .click('//*[@id="main"]/nav/div/ul[3]/li[2]/button')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[1]/a')
+        .assert.elementPresent('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[3]/a')
+        .end();
+    });
+
 });
